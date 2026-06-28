a3dde108b295e3f7f21305d3ac3ab651492c60e1
Winner luyeye <winnerluyeye1@gmail.com>
2026-06-28 23:08:31 +0100

    fix(config): ajout de Cloudinary dans les remotePatterns de Next.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   images: {
     domains: ['localhost'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/dukqurtfw/**', // Permet tous les chemins sous ce dossier
+      },
+    ],
   },
   typescript: {
     ignoreBuildErrors: false,
